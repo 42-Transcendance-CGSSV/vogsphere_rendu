9f2fa3a7b6e67f02e805d83a1d9e013e0c13ad6c
vSKAH <vskahhh@gmail.com>
2025-06-13 15:25:03 +0200

    update launcher

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 COMPOSE_FILE="docker-compose.yml"
-DOCKER_COMPOSE="docker compose -f $COMPOSE_FILE"
-DOCKER_BUILDKIT="DOCKER_BUILDKIT=1"
-COMPOSE_DOCKER_CLI_BUILD="COMPOSE_DOCKER_CLI_BUILD=1"
+DOCKER_COMPOSE="docker compose --file $COMPOSE_FILE"
 
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
@@ -38,9 +36,8 @@ function check_env() {
   fi
 
   local required_vars=(
-    BACKEND_SECRET_KEY JWT_SECRET_KEY TEMPORARY_JWT_SECRET_KEY PYTHONUNBUFFERED
-    VITE_CLIENT_ID VITE_CLIENT_SECRET SUPERUSER_USERNAME SUPERUSER_EMAIL
-    SUPERUSER_PASSWORD DB_NAME DB_USER DB_PASSWORD
+    ENVIRONMENT LOG_LEVEL BREVO_API_KEY
+    JWT_SECRET
   )
 
   for var in "${required_vars[@]}"; do
@@ -51,6 +48,8 @@ function check_env() {
   done
 
   echo -e "${GREEN}Environment variables check passed${NC}"
+  IP_ADDRESS=$(ip addr | awk '/inet / {if (++n==2) print $2}' | cut -d/ -f1)
+  echo -e "${YELLOW}Detected IP: ${IP_ADDRESS}${NC}"
 }
 
 function setup_ssl() {
@@ -60,34 +59,23 @@ function setup_ssl() {
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
       -keyout nginx/web_server/ft_transcendence.key -out nginx/web_server/ft_transcendence.crt \
       -subj "/C=FR/ST=AURA/L=Lyon/O=42/OU=Ft_transcendence/CN=127.0.0.1"
+    echo -e "${GREEN}SSL Certificate generated !${NC}"
+  else
+    echo -e "${GREEN}SSL Certificate is already present !${NC}"
   fi
   chmod 644 nginx/web_server/ft_transcendence.crt
   chmod 600 nginx/web_server/ft_transcendence.key
 }
 
-function setup_env() {
-  IP_ADDRESS=$(ip addr | awk '/inet / {if (++n==2) print $2}' | cut -d/ -f1)
-  echo -e "${YELLOW}Detected IP: ${IP_ADDRESS}${NC}"
-
-  GAME_TOKEN=$(openssl rand -hex 32)
-
-  sed -i '/^AI_SERVICE_TOKEN/d;/^GAME_SERVICE_TOKEN/d;/^VITE_REDIRECT_URI/d' .env
-
-  echo "GAME_SERVICE_TOKEN=Bearer $GAME_TOKEN" >> .env
-  echo "VITE_REDIRECT_URI=https://$IP_ADDRESS:7777/auth/authfortytwo" >> .env
-
-}
-
 function build() {
   check_env
   setup_ssl
-  setup_env
   if [ "$1" ]; then
     echo -e "${GREEN}Building $1...${NC}"
-    $DOCKER_BUILDKIT $COMPOSE_DOCKER_CLI_BUILD $DOCKER_COMPOSE build "$1"
+    $DOCKER_COMPOSE --build "$1"
   else
     echo -e "${GREEN}Building all services...${NC}"
-    $DOCKER_BUILDKIT $COMPOSE_DOCKER_CLI_BUILD $DOCKER_COMPOSE build
+    $DOCKER_COMPOSE build
   fi
 }
 
@@ -128,14 +116,12 @@ function clean() {
   down
   docker builder prune -f
   $DOCKER_COMPOSE down -v
-  rm -rf ssl
-  sed -i '/AI_SERVICE_TOKEN/d;/GAME_SERVICE_TOKEN/d;/VITE_REDIRECT_URI/d' .env
+  rm -rf nginx/web_server/ft_transcendence.crt && rm -rf nginx/web_server/ft_transcendence.key
 }
 
 
 function rebuild() {
   setup_ssl
-  setup_env
   if [ "$1" ]; then
     $DOCKER_COMPOSE up --build "$1"
   else
@@ -147,9 +133,7 @@ function rebuild() {
 case "$1" in
   help|"") help ;;
   build) shift; build "$@" ;;
-  build-fast) shift; build_fast "$@" ;;
   up) up ;;
-  up-fg) up_fg ;;
   down) down ;;
   stop) stop ;;
   restart) shift; restart "$@" ;;
@@ -157,10 +141,7 @@ case "$1" in
   ps) ps_containers ;;
   clean) clean ;;
   setup-ssl) setup_ssl ;;
-  setup-env) setup_env ;;
-  update-hashes) update_hashes ;;
+  check-env) check_env ;;
   rebuild) shift; rebuild "$@" ;;
-  rebuild-fast) shift; rebuild_fast "$@" ;;
-  rebuild-fg) shift; rebuild_fg "$@" ;;
-  *) echo "Commande inconnue: $1. Tapez ./dev.sh help" ;;
+  *) echo "Commande inconnue: $1. Tapez ./launcher.sh help" ;;
 esac
